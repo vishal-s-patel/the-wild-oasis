dc5cbcbbaebc86e578fe5f4b81dfddb532e295e0
vishal-s-patel <vishalpatel1002@yahoo.com>
2024-09-29 14:44:09 +0530

    fix: removed duplicate readme file

--- src/services/supabase.js
+++ src/services/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
-export const supabaseUrl = "https://jifttlxfvovqwxgohoju.supabase.co";
-const supabaseKey =
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImppZnR0bHhmdm92cXd4Z29ob2p1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjYxNTkxMDIsImV4cCI6MjA0MTczNTEwMn0.Y87OBoQWgu0IXRgTPTXG1-hGbC1EFoMFGklCAWgmhkc";
+export const supabaseUrl = import.meta.env.VITE_API_URL;
+const supabaseKey = import.meta.env.VITE_API_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
